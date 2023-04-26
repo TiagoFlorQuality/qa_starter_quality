65daf0bd6989b5b3e33eda3a0c815636a1509a3b
TiagoFlorQuality <tiago.flor@quality.com.br>
2023-04-26 10:38:12 -0300

    commit exercicio 1

---
+++ cypress/e2e/examples/login.feature
@@ -0,0 +1,8 @@
+Feature: Login
+
+    Scenario: Login Access
+        #DADO = GIVEN / QUANDO = WHEN / AND = E / THEN = ENTAO
+        Given Que Esteja na Homepage 
+        When Acessar a Página de Login
+        And Digitar Credenciais Erradas e Clicar no Submit
+        Then Mensagem de Erro Deve Aparecer
--- cypress/support/step_definitions/steps.js
+++ cypress/support/step_definitions/steps.js
@@ -8,3 +8,22 @@ When("I search for Grupo Quality", () => {
     cy.get('[title="Pesquisar"]').type('Quality Digital{enter}')
 })
 
+// Cenário do Exercicio Cypress
+
+Given('Que Esteja na Homepage', () => {
+    cy.visit("https://automationexercise.com/")
+});
+
+When('Acessar a Página de Login', () => {
+    cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
+});
+
+And('Digitar Credenciais Erradas e Clicar no Submit', () => {
+    cy.get('[data-qa="login-email"]').type("teste@login.com.br")
+    cy.get('[data-qa="login-password"]').type("Teste#123321",{log: false})
+    cy.get('[data-qa="login-button"]').click()
+});
+
+Then('Mensagem de Erro Deve Aparecer', () => {
+    cy.get('.login-form > form > p').should('have.text', "Your email or password is incorrect!")
+});
